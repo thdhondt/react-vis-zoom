76de5b2f24faa78194a0205b9d9e27a3d051f3aa
D'hondt Thomas <thdhondt@ulb.ac.be>
2017-12-12 00:00:21 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import ZoomableChartExample from './zoomable-chart-example'
+
+render(<ZoomableChartExample />, document.getElementById("root"));
